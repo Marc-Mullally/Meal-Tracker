439dc17cd01f4583e0fb8df78e04b12b989e002f
Marc Mullally <marcmullally0@gmail.com>
2024-02-27 12:59:53 -0500

    Initial Commit

---
+++ Meal Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meal Tracker
+//
+//  Created by 2b on 2/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
